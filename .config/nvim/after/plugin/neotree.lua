6ce3ac42c11f232a1b5615587efba1b83e6f7e1a
Ji Park <61026408+jpnws@users.noreply.github.com>
2023-01-08 16:57:36 -0800

    Update neotree config.

--- .config/nvim/after/plugin/neotree.lua
+++ .config/nvim/after/plugin/neotree.lua
@@ -1,4 +1,10 @@
 require("neo-tree").setup {
+    filesystem = {
+        filtered_items ={
+            hide_dotfiles = false,
+            hide_hidden = true,
+        },
+    },
     window = {
         mappings = {
             -- Disable <space> key.
